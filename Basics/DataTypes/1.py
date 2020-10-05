8af90304def115014071411dd24e7560ea7725a6
Saravanan <bprathap104@gmail.com>
2020-10-05 16:31:11 +0530

    Initial commit

---
+++ Basics/DataTypes/1.py
@@ -0,0 +1,47 @@
+""""
+Integers
+"""
+a = 3
+c = 0b101010111
+print(c)
+d = 0o454312
+print(d)
+e = 0xac4d
+print(e)
+f = '12345'
+print(f)
+print(int(f))
+print(type(a))
+""""
+Floating point numbers
+Division always gives float numbers
+"""
+a = 10
+b = 4
+print(a / b)
+print(a // b)
+j=4e3
+print(j)
+print(4e-3)
+a = 0.2
+b =0.1
+c = a + b
+print(c)
+""""
+Complex Numbers
+"""
+
+a = 2+3j
+b = 4+5j
+print(type(a))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+""""
+String
+"""
+a = 'He said "i wasn\'t available for class today"'
+print(a)
+b=r"One Line\nTwo Line"
+print(b)
