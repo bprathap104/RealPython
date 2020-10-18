80d7c50c5bda877a3b0d64e42644bf4ba8addb18
Saravanan <bprathap104@gmail.com>
2020-10-18 06:32:14 +0530

    basic data types

--- Basics/DataTypes/1.py
+++ Basics/DataTypes/1.py
@@ -45,3 +45,128 @@ a = 'He said "i wasn\'t available for class today"'
 print(a)
 b=r"One Line\nTwo Line"
 print(b)
+"""
+Triple-quoted-String
+"""
+
+
+"""
+Booleans
+"""
+a = 2
+print(type(a))
+print(bool(a))
+
+a = 2
+print(a == 2)
+b = 2 
+print(b == 0)
+if b:
+    print('B is true!')
+else:
+    print('B is false!')
+
+b = False 
+if b:
+    print('B is true!')
+else:
+    print('B is false!')
+
+b = [] 
+if b:
+    print('B is true!')
+else:
+    print('B is false!')
+
+b = {} 
+if b:
+    print('B is true!')
+else:
+    print('B is false!')
+
+
+"""
+Functions - Composite Data Types
+List & Tuple
+"""
+a = [1,2,3,4,5]   ## List
+print(a[-1])
+print(a[1:3])
+a[1] = 6
+print(a)
+
+b = (1,2,3,4,5)   ## Tuple
+print(b[0])
+print(b[-1])
+print(b[1:3])
+## b[1] = 6  ## This is not allowed in tuple
+"""
+Functions - Composite Data Types
+Set - A set is a data structure that can contain only unique elements
+"""
+a = [1,2,3,3,3,4]
+print(a)
+b = set(a)
+print(b)
+c=list(b)
+print(c)
+
+"""
+Functions - Composite Data Types
+Dict - Lookup table
+"""
+a = dict()
+a['age'] = 48
+a['name'] = 'Darren'
+print(a)
+print(a['age'])
+## print(a['country']) # This will give a key error as 'country' is not defined
+print(a.get('country'))
+
+
+"""
+Functions - Composite Data Types
+Math
+"""
+print(abs(5))
+print(abs(-5))
+print(15 // 4)
+print(15 % 4)
+print(divmod(15, 4))
+print(max([1,2,3,4,5]))
+print(max([-4,-2]))
+
+print(min([1,2,3]))
+print(min([-4,-2]))
+
+print(pow(3,2))
+print(3 ** 2)
+## pow(3,2,2) = 3 ** 2 % 2
+print(pow(3,2,2))
+print(round(4.5))
+print(round(4.6))
+print(int(4.5))
+print(int(4.6))
+print(sum([1,2,3,4,5]))
+print(sum([1,2,3,4,5],10))
+
+"""
+Functions - Type Conversion
+"""
+print(ascii("hello"))      ## ASCII -> Americal standard code for Information Interchange
+print(ascii("'ôæ"))
+print(chr(244))            ## chr works for both ascii & unicode characters
+print(chr(0x06a4))
+print(ord('a'))              ## ord is complementary to chr function. ord does convert any character into ascii
+print(type('a'))
+print(type(2))
+print(type('hello'))
+print(type(4.0))
+print(type(True))
+print(type([1,2,3,4,5]))
+print(int('1234'))
+print(int('1234', base=16))
+print(int('1010', base=2))
+print(bin(10))
+print(oct(10))
+print(hex(10))
